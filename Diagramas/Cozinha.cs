b153c1de22597a35c7bfbed02b59bd717ac0402d
MetheusFerreira <84148229+MetheusFerreira@users.noreply.github.com>
2021-08-27 13:53:53 -0300

    Create Cozinha.cs

---
+++ Diagramas/Cozinha.cs
@@ -0,0 +1,10 @@
+public class Cozinha
+{
+    private PortaCozinha porta;
+    private bool americana;
+    private float metragemQuadrada;
+
+    public PortaCozinha Porta { get => porta; set => porta = value; }
+    public bool Americana { get => americana; set => americana = value; }
+    public float MetragemQuadrada { get => metragemQuadrada; set => metragemQuadrada = value; }   
+}

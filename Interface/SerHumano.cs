fc9bef6bdc291e03a552fafb093ad0694e8fe060
MetheusFerreira <84148229+MetheusFerreira@users.noreply.github.com>
2021-08-27 14:01:11 -0300

    Create SerHumano.cs

---
+++ Interface/SerHumano.cs
@@ -0,0 +1,11 @@
+public class SerHumano : Animal, IRacional{
+    public override void Falar(){
+        System.Console.WriteLine("Fabiano Chu Passeios");
+    }
+    public override void Latir(){
+        System.Console.WriteLine("Deide Costa");
+    }
+    public void Andar(){
+        System.Console.WriteLine("Jalim Habey");
+    }
+}

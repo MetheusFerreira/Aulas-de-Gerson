6b086459b168120cdd79089725e7d6d9bbf03ac7
MetheusFerreira <84148229+MetheusFerreira@users.noreply.github.com>
2021-08-27 14:27:12 -0300

    Update main.cs

--- Herança/main.cs
+++ Herança/main.cs
@@ -6,9 +6,9 @@ class MainClass {
     ContaCorrente c2 = new ContaCorrente("Paulo Ricardo", 1235);
 
 	c2.Creditar(100);
-    c1.Creditar(250);
-    c1.AdicionarRendimento(5);
-    c2.CobrarTaxa(1);
+	c1.Creditar(250);
+	c1.AdicionarRendimento(5);
+    	c2.CobrarTaxa(1);
 
     Console.WriteLine(c1.Saldo);
     Console.WriteLine(c2.Saldo);

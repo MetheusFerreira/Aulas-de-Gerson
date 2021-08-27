fece26668dcfd0c1cbe73d9f36add6c34210718f
MetheusFerreira <84148229+MetheusFerreira@users.noreply.github.com>
2021-08-27 08:43:12 -0300

    Update DateTimeAtt.cs

--- DateTimeAtt.cs
+++ DateTimeAtt.cs
@@ -8,7 +8,7 @@ class Program {
     DateTime agora = DateTime.Now;
     TimeSpan dif = agora - data;
     Console.WriteLine($"Você tem entre {agora.Year - data.Year} e {agora.Year - data.Year - 1} anos de idade.");
-    Console.WriteLine("Já vivesses " + dif.ToString("dd") + " dias.");
-    Console.WriteLine("Já vivesses " + dif.TotalHours + " horas.");
+    Console.WriteLine($"Já vivesses {dif.ToString("dd")} dias.");
+    Console.WriteLine($"Já vivesses {dif.TotalHours} horas.");
   }
 }
